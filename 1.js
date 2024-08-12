97e15524da366bda06703d9c4b6c14ee26f5bf05
Muhammad Ahmad <ahmad321ali007@gmail.com>
2024-08-12 22:31:42 +0500

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,35 @@
+var Name = "Muhammad Ahmad";
+console.log(Name);
+// operators
+// ARITHMETIC OPERATORS
+var a = 7;
+console.log(a * 10);
+console.log(a / 7);
+console.log(a + 3);
+console.log(a - 7);
+// LOFICAL OPERATORS
+// &&= AND , || = OR , != NOT
+// ASSIGNMENT OPERATORS
+var passingnumbers = 70;
+console.log(passingnumbers += 0);
+var myage = 16;
+console.log(myage += 1.5);
+// INCREMENT & DECREMENT OPERATORS
+var age = 17;
+var plus = 0.5;
+console.log(age++ + plus);
+var c = 7;
+c--;
+console.log(c);
+// COMPARISON OPERATORS
+var cars = 10;
+var bikes = 7;
+console.log(cars < bikes);
+console.log(cars > bikes);
+var cookies = 10;
+var candies = 10;
+console.log(cookies >= candies);
+var y = 10;
+console.log(y == 10);
+// CONDITIONAL OPERATORS
+// ?, :
---
+++ 1.ts
@@ -0,0 +1,62 @@
+let Name:string = "Muhammad Ahmad"
+console.log(Name);
+
+
+// operators
+
+// ARITHMETIC OPERATORS
+let a = 7;
+
+console.log(a * 10);
+console.log(a / 7);
+console.log(a + 3);
+console.log(a - 7);
+
+// LOFICAL OPERATORS
+
+// &&= AND , || = OR , != NOT
+
+// ASSIGNMENT OPERATORS
+
+let passingnumbers = 70;
+
+console.log(passingnumbers += 0);
+
+let myage = 16;
+
+console.log(myage += 1.5);
+
+// INCREMENT & DECREMENT OPERATORS
+
+var age = 17;
+var plus = 0.5;
+
+console.log(age++ + plus);
+
+let c = 7;
+c-- 
+console.log(c);
+
+// COMPARISON OPERATORS
+
+let cars = 10;
+let bikes = 7;
+console.log(cars < bikes);
+console.log(cars > bikes);
+
+let cookies = 10;
+
+let candies = 10;
+
+console.log(cookies >= candies );
+
+let y = 10;
+
+console.log(y == 10);
+
+// CONDITIONAL OPERATORS
+// ?, :
+
+
+
+
